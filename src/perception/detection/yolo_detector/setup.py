f80bc401bd036fe3049f326b059e74e61ccd4f29
Jacob Zhong <cmpute@qq.com>
2019-07-05 16:38:18 -0400

    Fix yolov3 as submodule

--- src/perception/detection/yolo_detector/setup.py
+++ src/perception/detection/yolo_detector/setup.py
@@ -4,8 +4,10 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['yolov3'],
-    package_dir={'': 'src'}
+    packages=['yolov3', 'yolov3.utils'],
+    package_dir={'': 'src'},
+    package_data={'yolov3': ['src/yolov3/cfg/*', 'src/yolov3/data/*', 'src/yolov3/weights/*']},
+    install_requires={'torch', 'pathlib', 'google-cloud-storage'}
 )
 
 setup(**d)
