3d4d2594f8d1ecf119763d88ee079ed88ff58ac7
Jacob Zhong <cmpute@qq.com>
2019-07-09 11:34:19 -0400

    Migrated static map generator

--- src/control/preview_control/src/zzz_control_preview_control/controller_preview_control.py
+++ src/control/preview_control/src/zzz_control_preview_control/controller_preview_control.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2018 Intel Labs.
-# authors: German Ros (german.ros@intel.com)
-#
-# This work is licensed under the terms of the MIT license.
-# For a copy, see <https://opensource.org/licenses/MIT>.
-
 """ This module contains PID controllers to perform lateral and longitudinal control. """
 
 from collections import deque
--- src/navigation/map_provider/sumo/setup.py
+++ src/navigation/map_provider/sumo/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['zzz_driver_simulators_carla_adapter'],
+    packages=['zzz_navigation_map_provider_sumo'],
     package_dir={'': 'src'}
 )
 
