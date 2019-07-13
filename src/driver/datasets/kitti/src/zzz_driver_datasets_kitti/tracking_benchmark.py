de0e4f0d4a746c70a45b15569eb96a5de6d064d1
Jacob Zhong <cmpute@qq.com>
2019-07-13 14:43:23 -0400

    [Temp]

---
+++ src/driver/datasets/kitti/src/zzz_driver_datasets_kitti/tracking_benchmark.py
@@ -0,0 +1,16 @@
+
+
+class TrackingBenchmark:
+    """
+    Benchmark results target on KITTI/Tracking and KITTI/Raw dataset.
+    """
+    def __init__(self):
+        pass
+
+    def add_frame(self, result, gt):
+        '''
+        Add result from one frame. Result and GT field are expected to be type of zzz_percetion_msgs.msg.TrackingBoxArray
+        '''
+        pass
+
+    pass
