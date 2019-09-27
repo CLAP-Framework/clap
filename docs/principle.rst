39386d03f51b33ada985972bc90250950637b56c
Jacob Zhong <cmpute@qq.com>
2019-09-27 15:00:53 -0400

    Reorganized ipm projection and add calib tool

--- docs/principle.rst
+++ docs/principle.rst
@@ -54,11 +54,15 @@ Frames definition
 
 Each sensor has an attached frame, and the extrinsic calibration result should be published to `/tf` as TransformStamped between the sensors. Then some common frames are defined listed below:
 
-- `base_link`: attached at geometry center of ego vehicle with FLU coordinate. The transform between frame of other sensors and `base_link` is usually static. (published as `tf_static`)
+- `base_link`: attached at somewhere (e.g. the geometry center) of ego vehicle body with FLU coordinate. The transform between frame of other sensors and `base_link` is usually static. (published as `tf_static`)
+- `base_chassis`: attached at rear axis center of ego vehicle with FLU coordinate. Since this frame is attached under suspension, the transform between `base_link` and `base_body` is actualy dynamic, but we usually ignore it.
+- `base_footprint`: attached at the projection of rear axis center on ground with projected FLU coordinate, which is a FLU coordinate projected on ground. X-Y plane of the projected coordinate is aligned with tangent plane of the road.
 - `odom`: a coordinate originated at the start point of the vehicle (both position and orientation is fixed to). The transform between `base_link` and `odom` will be updated whenever a new odometry estimation is performed.
 - `map`: frame with a assigned origin and the axis should be aligned with the ground coordinate (ENU). The transform between `odom` and `map` will be updated when the map area is updated or odometry drift is corrected.
 
 So the transform between sensors and between `map` and `odom` are almost static, while the transform between `odom` and `base_link` are frequently updated with `nav_msgs/Odometry` messages from perception module. Transform between `map` and `odom` will being compensated by a perception or navigation module regularly. If needed, the sensor transform could also be adjusted online regularly.
+
+Also note that the coordinate attached to the cameras follows [standard definition of ROS](http://wiki.ros.org/image_pipeline/CameraInfo)
 
 
 ROS Namespaces
---
+++ src/tools/calibration/calibration_broadcaster.py
@@ -0,0 +1,148 @@
+'''
+This module read calibration settings from file and broadcast them by proper messages (Transform, CameraInfo, etc.)
+The content of the calibration file should contain following contents
+
+```yaml
+- The whole parameters are represented among `frame`s. A `frame` is a representation of 6D pose of a sensor.
+
+- extrinsic:
+  - This field represent extrinsic parameters between frames
+
+  - frame_id: frame name
+  - child_frame_id: child frame name
+  - txyz: 3-D translation from frame_id to child_frame_id
+  - qxyzw: 3-D rotation from frame_id to child_frame_id in quaternion
+  - valid_date: The extrinsic parameters are not always fixed, this field define which period the parameters are valid, in format 'yymmdd-yymmdd'
+
+- intrinsic:
+  - This field represent intrinsic parameters of sensors. Usually it's used to store camera intrinsics following ROS standard style
+
+  - frame_id: frame name of the sensor
+  - width: width of the image in pixels
+  - height: height of the image in pixels
+  - D: distortion coefficients of a camera (k1, k2, p1, p2, k3)
+  - K: 3x3 intrinsic matrix of a camera. Diagonals are fx, fy, 1, last column is cx, cy, 1
+  - P: 3x4 new projection matrix, convert coordinate from u,v to x,y,z
+  - R: 3x3 rectification transformation of stereo cameras
+```
+
+Currently only json format is supported. Example json:
+```json
+{
+    "extrinsics": [
+        {
+            "txyz": [
+                -0.02518118,
+                -0.14178041,
+                -0.5595681
+            ],
+            "qxyzw": [
+                0.50374569,
+                -0.50228743,
+                0.49632158,
+                0.49760678
+            ],
+            "frame_id": "vlp32_1",
+            "child_frame_id": "cam_fov60",
+            "valid_date": "181101-181129"
+        },
+    ],
+    "intrinsics": [
+        {
+            "frame_id": "cam_fov60",
+            "width": 1920,
+            "height": 1080,
+            "D": [
+                -0.5907738972958697,
+                0.3105447423625289,
+                -0.002406116369171268,
+                -0.004505115057455168,
+                0.0
+            ],
+            "K": [
+                2031.8389140381164, 0.0, 1007.2845434678806,
+                0.0, 2029.4242008945453, 503.65049480323916,
+                0.0, 0.0, 1.0
+            ],
+            "R": [
+                1.0, 0.0, 0.0,
+                0.0, 1.0, 0.0,
+                0.0, 0.0, 1.0
+            ],
+            "P": [
+                1718.5859375, 0.0, 1014.6422458803281, 0.0,
+                0.0, 1937.922119140625, 498.91599822248827, 0.0,
+                0.0, 0.0, 1.0, 0.0
+            ]
+        },
+    ]
+}
+```
+'''
+
+import json
+import time
+
+import rospy
+import tf.transformations as tft
+import tf2_ros as tf2
+
+from sensor_msgs.msg import CameraInfo
+from geometry_msgs.msg import TransformStamped
+
+############## NOTE: Change you settings here ###############
+calib_file = "demo.calib.json" # calibration file location
+caminfo_topic = "/caminfo" # this topic is used to contain aggregated intrinsics of camera, its behavior is like '/tf'
+pub_rate = 5 # frequency of publishing calibration data, unit is Hz
+#############################################################
+
+def create_transform(item):
+    transform = TransformStamped()
+    transform.header.stamp = rospy.Time.now()
+    transform.header.frame_id = item['frame_id']
+    transform.child_frame_id = item['child_frame_id']
+    transform.transform.translation.x = item['txyz'][0]
+    transform.transform.translation.y = item['txyz'][1]
+    transform.transform.translation.z = item['txyz'][2]
+    transform.transform.rotation.x = item['qxyzw'][0]
+    transform.transform.rotation.y = item['qxyzw'][1]
+    transform.transform.rotation.z = item['qxyzw'][2]
+    transform.transform.rotation.w = item['qxyzw'][3]
+    return transform
+
+def create_camera_info(item):
+    caminfo = CameraInfo()
+    caminfo.header.stamp = rospy.Time.now()
+    caminfo.header.frame_id = item['frame_id']
+
+    caminfo.width = item['width']    
+    caminfo.height = item['height']
+    caminfo.distortion_model = 'plumb_bob'
+
+    caminfo.K = list(item['K'])
+    caminfo.D = list(item['D'])
+    caminfo.R = list(item['R'])
+    caminfo.P = list(item['P'])
+
+    return caminfo
+
+if __name__ == '__main__':
+    calib_params = None
+    with open(calib_file, 'r') as fin:
+        calib_params = json.load(fin)
+
+    rospy.init_node('calib_broadcast', anonymous=True)
+    tfreporter = tf2.TransformBroadcaster()
+    camreporter = rospy.Publisher("/caminfo", CameraInfo)
+
+    rate = rospy.Rate(pub_rate)
+    while True:
+        try:
+            for extri in calib_params['extrinsics']:
+                tfreporter.sendTransform(create_transform(extri))
+            for intri in calib_params['intrinsics']:
+                camreporter.publish(create_camera_info(intri))
+
+            rate.sleep()
+        except rospy.exceptions.ROSTimeMovedBackwardsException:
+            continue
