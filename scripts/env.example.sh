648ce37180bb53c01acf4f5f558721880256e428
Jacob Zhong <cmpute@qq.com>
2019-12-19 17:33:12 -0500

    Add launch files

---
+++ scripts/env.example.sh
@@ -0,0 +1,5 @@
+export ZZZ_HOME=/home/jacobz/ZZZ
+export SUMO_HOME=/usr/share/sumo
+export CARLA_ROOT=/home/jacobz/Carla/Carla_0.9.6
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.6-py2.7-linux-x86_64.egg
+. $ZZZ_HOME/devel/setup.sh
