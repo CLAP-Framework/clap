6983590aa6b5658f7259eea83fc124797c76d865
zhcao <caoc11@foxmail.com>
2019-12-20 19:08:14 +0800

    Revert "adjust_planning"
    This reverts commit 8e07c7bb55a476a86f55c2d20a8cdeee1d1ea30e.

--- src/cognition/object_locator/src/zzz_cognition_object_locator/driving_space_constructor.py
+++ src/cognition/object_locator/src/zzz_cognition_object_locator/driving_space_constructor.py
@@ -95,6 +95,7 @@ class DrivingSpaceConstructor:
             self.locate_speed_limit_in_lanes()
 
         #visualization
+        #1. lane center line
         self._lanes_markerarray = MarkerArray()
 
         count = 0
@@ -122,7 +123,35 @@ class DrivingSpaceConstructor:
             self._lanes_markerarray.markers.append(tempmarker)
             count = count + 1
 
+        #2. lane boundary line
+        self._lanes_markerarray = MarkerArray()
 
+        count = 0
+        for lane in self._static_map.lanes:
+            tempmarker = Marker() #jxy: must be put inside since it is python
+            tempmarker.header.frame_id = "map"
+            tempmarker.header.stamp = rospy.Time.now()
+            tempmarker.ns = "zzz/cognition"
+            tempmarker.id = count
+            tempmarker.type = Marker.LINE_STRIP
+            tempmarker.action = Marker.ADD
+            tempmarker.scale.x = 0.12
+            tempmarker.color.r = 1.0
+            tempmarker.color.g = 1.0
+            tempmarker.color.b = 1.0
+            tempmarker.color.a = 0.5
+            tempmarker.lifetime = rospy.Duration(0.5)
+
+            for lanepoint in lane.central_path_points:
+                p = Point()
+                p.x = lanepoint.position.x
+                p.y = lanepoint.position.y
+                p.z = lanepoint.position.z
+                tempmarker.points.append(p)
+            self._lanes_markerarray.markers.append(tempmarker)
+            count = count + 1
+
+        #3. obstacle
         self._obstacles_markerarray = MarkerArray()
         
         count = 0
@@ -181,7 +210,7 @@ class DrivingSpaceConstructor:
                     self._obstacles_markerarray.markers.append(tempmarker)
                     count = count + 1
 
-        #ego vehicle visualization
+        #4. ego vehicle
         self._ego_markerarray = MarkerArray()
 
         tempmarker = Marker()
