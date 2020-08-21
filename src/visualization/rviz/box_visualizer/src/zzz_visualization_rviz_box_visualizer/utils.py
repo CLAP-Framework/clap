872b98474a7450a977f1f27e032bc294328d4578
LIXIANG <xiang.li2@novauto.com.cn>
2020-08-21 17:53:03 +0800

    1. use corner to determine if a car is in lane now. 2. add 1 new class for drawing rviz information 3. add 2 topic for visualize front vehicle and its corner-to-lane distance 4. add 2 new function in zzz_common.utils 5. fix msg description of dimension

--- src/visualization/rviz/box_visualizer/src/zzz_visualization_rviz_box_visualizer/utils.py
+++ src/visualization/rviz/box_visualizer/src/zzz_visualization_rviz_box_visualizer/utils.py
@@ -1,5 +1,7 @@
 import rospy
 from std_msgs.msg import ColorRGBA
+from visualization_msgs.msg import Marker, MarkerArray
+from geometry_msgs.msg import Point
 
 def check_color(value):
     if value > 255.:
@@ -28,3 +30,82 @@ def parse_color(in_color):
     else:
         rospy.logerr("Cannot resolve color value. Check configuration please!")
     return color
+
+class RvizVisualizer():
+
+    @staticmethod
+    def draw_two_points_line(point1, point2, width=1, duration=0.5):
+        tempmarkerarray = MarkerArray()
+        tempmarker = Marker()
+        tempmarker.header.frame_id = "map"
+        tempmarker.header.stamp = rospy.Time.now()
+        tempmarker.ns = "zzz/decision"
+        tempmarker.id = 1
+        tempmarker.type = Marker.LINE_STRIP
+        tempmarker.action = Marker.ADD
+        tempmarker.scale.x = width
+        start_point = Point()
+        start_point.x = point1[0]
+        start_point.y = point1[1]
+        start_point.z = 2
+        end_point = Point()
+        end_point.x = point2[0]
+        end_point.y = point2[1]
+        end_point.z = 2
+        tempmarker.points.append(start_point)
+        tempmarker.points.append(end_point)
+        tempmarker.color.r = 0.0
+        tempmarker.color.g = 1.0
+        tempmarker.color.b = 1.0
+        tempmarker.color.a = 1
+        tempmarker.lifetime = rospy.Duration(duration)
+        tempmarkerarray.markers.append(tempmarker)
+        return tempmarkerarray
+
+
+
+    @staticmethod
+    def draw_circle(center_x, center_y, radius, duration=0.5):
+        tempmarkerarray = MarkerArray()
+        tempmarker = Marker()
+        tempmarker.header.frame_id = "map"
+        tempmarker.header.stamp = rospy.Time.now()
+        tempmarker.ns = "zzz/decision"
+        tempmarker.id = 1
+        tempmarker.type = Marker.CYLINDER
+        tempmarker.action = Marker.ADD
+        tempmarker.scale.x = radius
+        tempmarker.scale.y = radius
+        tempmarker.scale.z = radius
+        tempmarker.color.r = 0.0
+        tempmarker.color.g = 1.0
+        tempmarker.color.b = 0.5
+        tempmarker.color.a = 0.5
+        tempmarker.lifetime = rospy.Duration(duration)
+        tempmarker.pose.position.x = center_x
+        tempmarker.pose.position.y = center_y
+        tempmarkerarray.markers.append(tempmarker)
+        return tempmarkerarray
+
+    @staticmethod
+    def draw_words(text, pos_x, pos_y, scale=0.5, duration=0.5):
+        tempmarkerarray = MarkerArray()
+        tempmarker = Marker()
+        tempmarker.header.frame_id = "map"
+        tempmarker.header.stamp = rospy.Time.now()
+        tempmarker.ns = "zzz/decision"
+        tempmarker.id = 1
+        tempmarker.type = Marker.TEXT_VIEW_FACING
+        tempmarker.action = Marker.ADD
+        tempmarker.scale.z = scale
+        tempmarker.color.r = 0.0
+        tempmarker.color.g = 1.0
+        tempmarker.color.b = 0.5
+        tempmarker.color.a = 1
+        tempmarker.lifetime = rospy.Duration(duration)
+        tempmarker.pose.position.x = pos_x
+        tempmarker.pose.position.y = pos_y
+        tempmarker.pose.position.z = 2
+        tempmarker.text = text
+        tempmarkerarray.markers.append(tempmarker)
+        return tempmarkerarray
