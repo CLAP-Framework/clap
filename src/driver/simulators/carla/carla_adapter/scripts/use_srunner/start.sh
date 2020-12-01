8feb5e7b264cc14a0fdbb667598ec9371707d5b1
zzz <caoc11@foxmail.com>
2020-12-01 16:15:14 +0800

    1201-master with bridge

--- src/driver/simulators/carla/carla_adapter/scripts/use_srunner/start.sh
+++
@@ -1,4 +0,0 @@
-# This script is for Carla scenario_runner
-
-. $ZZZ_ROOT/zzz/devel/setup.sh
-roslaunch $ZZZ_ROOT/zzz/src/driver/simulators/carla/carla_adapter/scripts/use_srunner/main.launch # >zzz.log 2>zzz.err
